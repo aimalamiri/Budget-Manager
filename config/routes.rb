830d8cb0f80cda07b4a88d3947f25bcc492d434a
Aimal Amiri <meaimal@hotmail.com>
2022-09-05 18:00:13 +0430

    Add devise view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
