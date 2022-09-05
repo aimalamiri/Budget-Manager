b821a84271f9d4b958726c538f1422db55a34006
Aimal Amiri <meaimal@hotmail.com>
2022-09-05 17:19:20 +0430

    Add validations to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :records
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :icon, presence: true, length: { maximum: 255 }
 end
