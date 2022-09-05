df24c4346df7f7be3ff8646f66035eec2cbc3a8a
Aimal Amiri <meaimal@hotmail.com>
2022-09-05 14:46:16 +0430

    Rename truckage to record

--- db/migrate/20220905095607_create_records.rb
+++ db/migrate/20220905095607_create_records.rb
@@ -1,6 +1,6 @@
-class CreateTruckages < ActiveRecord::Migration[7.0]
+class CreateRecords < ActiveRecord::Migration[7.0]
   def change
-    create_table :truckages do |t|
+    create_table :records do |t|
       t.string :name
       t.integer :amount
       t.belongs_to :user, foreign_key: true, index: true
