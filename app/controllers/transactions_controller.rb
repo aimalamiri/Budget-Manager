caa166a1cff4073953723525384954ee2108a0e5
Aimal Amiri <meaimal@hotmail.com>
2022-09-07 13:38:07 +0430

    Add new transaction form page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,9 @@ class TransactionsController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
   end
+
+  def new
+    @category = Category.find(1)
+    @transaction = Record.new
+  end
 end
