c0cf129e5b85cc2b320350f267709abf9b93b748
Aimal Amiri <meaimal@hotmail.com>
2022-09-07 12:20:38 +0430

    Make categories page as home page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
 
   resources 'categories', only: %w[index new create]
 end
