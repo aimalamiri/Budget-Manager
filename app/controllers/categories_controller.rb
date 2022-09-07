b32c4eb9391ea7565ca828f0a2cf25c86e583eb5
Aimal Amiri <meaimal@hotmail.com>
2022-09-07 18:03:11 +0430

    Fix the categories user related bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
   def new
