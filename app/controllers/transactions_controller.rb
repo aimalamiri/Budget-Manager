1ce7b8d88ecd2c356571bc90a4043fe190124e32
Aimal Amiri <meaimal@hotmail.com>
2022-09-07 14:13:59 +0430

    Add create transaction for a category functionality

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,30 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @category = Category.find(params[:category_id])
   end
 
   def new
-    @category = Category.find(1)
+    @category = Category.find(params[:category_id])
     @transaction = Record.new
+  end
+
+  def create
+
+    @record = current_user.records.new(record_params)
+
+    if @record.save
+      @record_category = RecordCategory.create(category_id: params[:category_id], record_id: @record.id)
+      redirect_to category_transactions_path @record_category.category_id, flash: { success: "#{@record.name} has been successfully created!" }
+    else
+      redirect_to new_category_transaction_path @record_category.category_id, flash: { error: @record.errors.full_messages }
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:name, :amount)
   end
 end
