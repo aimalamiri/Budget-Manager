66076b92ef83adb6f1bb96be3e2458e6cf5499e7
Aimal Amiri <meaimal@hotmail.com>
2022-09-08 13:07:04 +0430

    Add test for the new category link in categories page

--- spec/views/categories_spec.rb
+++ spec/views/categories_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe 'Categories Page', type: :system do
   before :all do
     User.create(name: 'user 1', email: 'user1@test.com', password: 'password').encrypted_password
     @user = User.last
-    @category = Category.create(name: 'Car', icon: 'fa-car', user: @user)
+    @category = Category.create(name: "car#{rand(10000)}", icon: 'fa-car', user: @user)
+  end
+
+  before :each do
     sign_in @user
   end
 
-  it 'has the page title' do
+  it 'has the category name' do
     visit '/'
     expect(page).to have_content(@category.name)
+  end
+
+  it 'it redirects the new category page when the user clicks on add new category' do
+    visit '/'
+    find('#new-category').click
+    expect(page).to have_content('Add a new Category')
   end
 end
